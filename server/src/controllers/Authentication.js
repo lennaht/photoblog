f670c51d25cc5e8c2df0697660454cd54af773d0
Lennaht <33487842+lennaht@users.noreply.github.com>
2018-01-11 16:16:16 +0100

    Include password encryption with bcrypt

--- server/src/controllers/Authentication.js
+++ server/src/controllers/Authentication.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
 const { User } = require('../models');
+const bcrypt = require('bcrypt');
 
 module.exports = {
 	async register (req, res) {
 		try {
-			const user = await User.create(req.body);
+			const hash = await bcrypt.hash(req.body.password, 10);
+			const user = await User.create({
+				username: req.body.username,
+				password: hash
+			});
 			res.send(JSON.stringify(user));
 		} catch (err) {
 			console.log(err);
