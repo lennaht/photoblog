e9985d264bebd5e797969b8f5f3e0e971e7f5c12
Lennaht <33487842+lennaht@users.noreply.github.com>
2018-01-11 17:52:37 +0100

    Created login route wich also sends a jwt token to the client

--- server/src/controllers/Authentication.js
+++ server/src/controllers/Authentication.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const { User } = require('../models');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const config = require('../../config');
 
 module.exports = {
 	async register (req, res) {
@@ -10,7 +12,7 @@ module.exports = {
 				username: req.body.username,
 				password: hash
 			});
-			res.send(JSON.stringify(user));
+			res.status(200).send(JSON.stringify(user));
 		} catch (err) {
 			console.log(err);
 			res.status(400).send({
@@ -20,5 +22,44 @@ module.exports = {
 			});
 		}
 		
+	},
+	async login (req, res) {
+		try {
+			const { username, password } = req.body;
+
+			const user = await User.findOne({'username': username});
+			if(!user) {
+				return res.status(403).send({
+					error: {
+						message: 'The login information was incorrect.'
+					}
+				});
+			}
+			console.log(user);
+			const isPasswordCorrect = await bcrypt.compare(password, user.password);
+			if (!isPasswordCorrect) {
+				return res.status(403).send({
+					error: 'The login information was incorrect.'
+				});
+			}
+
+			const token = await jwt.sign({
+				username: user.username,
+				userId: user._id
+			}, config.jwtKey, {expiresIn: 60 * 60});
+
+			res.status(200).send({
+				message: 'User logged in successfully',
+				token: token
+			});
+
+		} catch (err) {
+			res.status(500).send({
+				error: {
+					message: 'Oops, an error occured. Please try again.',
+					error: err
+				}
+			});
+		}
 	}
 };
--- server/src/routes.js
+++ server/src/routes.js
@@ -3,7 +3,9 @@ const auth = require('./controllers/Authentication');
 const validate = require('./validation/validateUser');
 
 module.exports = (app) => {
-	app.post('/register', validate.register, auth.register);	
+	app.post('/register', validate.register, auth.register);
+	
+	app.post('/login', auth.login);
 
 	app.get('/status', (req, res) => {
 		res.send('Back-end working');
